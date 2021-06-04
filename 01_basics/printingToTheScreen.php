3c99ec61e04b4cd471387acf117b519d94abe58a
Syransong <ksysong@gmail.com>
2021-06-03 17:41:14 -0700

    create folder for basics unit and printing files

---
+++ 01_basics/printingToTheScreen.php
@@ -0,0 +1,12 @@
+<!-- Echo Method -->
+<?php
+  echo 'Hello World';
+?>
+
+<!-- Shorthand echo Method(not recommended as it can be turned off on the server -->
+<?='Hellow World'?>
+
+<!-- Print method -->
+<?php
+  print 'Hello World'; 
+?>
